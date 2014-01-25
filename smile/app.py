d9a7aea73f036ccb2f14648b55b8814fb3269356
AmundsenJunior <scottedwardrussell@gmail.com>
2014-01-25 13:36:52 -0500

    Push of smile/app.py files to repo

---
+++ smile/app.py
@@ -0,0 +1,10 @@
+from rapidsms.apps.base import AppBase
+
+class Messaging(AppBase):
+
+	def handle(self, msg):
+		if msg.text == 'SMILE':
+			msg.respond('Al-Atqia Mosque, King Faisal Street, Hebron.\n15 February 2014, 0800-1700')
+			return True
+		return False
+
