e974d951e2a2b0cb22662fec73fa495579ffbeb7
AmundsenJunior <scottedwardrussell@gmail.com>
2014-01-25 14:41:34 -0500

    Added logic and content for handling sms requests from multiple locations

--- smile/app.py
+++ smile/app.py
@@ -1,10 +1,42 @@
 from rapidsms.apps.base import AppBase
+import string
 
 class Messaging(AppBase):
 
 	def handle(self, msg):
-		if msg.text == 'SMILE':
-			msg.respond('Al-Atqia Mosque, King Faisal Street, Hebron. 15 February 2014, 0800-1700')
+		msg_txt = string.upper(msg.text)
+		if msg_txt == 'SMILE':
+			msg.respond('Please reply with municipality.')
 			return True
-		return False
+		else:
+			location = string.lsplit(msg_txt, 'SMILE IN ')
+
+			if location == 'HEBRON':
+				msg.respond('Al-Atqia Mosque, King Faisal Street, Hebron. 15-20 February 2014, 0800-1700')
+				return True
+			elif location == 'NABLUS':
+				msg.respond('Fatimid School, Omar Ibn Al-Khattab Street, Nablus. 15-20 February 2014, 0800-1700')
+				return True
+			elif location == 'RAFAH':
+				msg.respond('Alrahma Mosque, Othman Bn Afaan, Rafah. 15-20 February 2014, 0800-1700')
+				return True
+			elif location == 'RAMALLAH':
+				msg.respond('Alrahman Mosque, Alsawsan Street, Ramallah. 15-20 February 2014, 0800-1700')
+				return True
+			elif location == 'GAZA':
+				msg.respond('Omary Mosque, Omar Al-Mukhtar Street, Gaza. 15-20 February 2014, 0800-1700')
+				return True
+			elif location == 'JERUSALEM':
+				msg.respond('Maha Pharmacy, Steah Street, Jerusalem. 15-20 February 2014, 0800-1700')
+				return True
+			elif location == 'BETHLEHEM':
+				msg.respond('Beit Jala Hospital, Madares Street, Bethlehem. 15-20 February 2014, 0800-1700')
+				return True
+			elif location == 'JERICHO':
+				msg.respond('Al Satafi Pharmacy, Jamal Abd Al-Naser, Jericho. 15-20 February 2014, 0800-1700')
+				return True
+			else:
+			# invalid or unknown locations
+			# flag to admin in DB for investigation and manual response
+				return False
 
