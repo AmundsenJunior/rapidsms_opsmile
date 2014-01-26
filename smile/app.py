4c94e59d36c59b3811868f865b023cad533ec714
AmundsenJunior <scottedwardrussell@gmail.com>
2014-01-26 12:42:43 -0500

    Altered app logic from string to list based. Better message comprehension, and more response options.

--- smile/app.py
+++ smile/app.py
@@ -4,13 +4,16 @@ import string
 class Messaging(AppBase):
 
 	def handle(self, msg):
-		msg_txt = string.upper(msg.text)
-		if msg_txt == 'SMILE':
+
+		msg_txt = msg.text.upper().split()
+		msg_len = len(msg_txt)
+
+		if msg_txt[0] == 'SMILE' and msg_len < 3:
 			msg.respond('Please reply with municipality.')
 			return True
-		else:
-			location = string.lstrip(msg_txt, 'SMILE IN ')
-
+		elif msg_txt[2:] != []:
+			location = string.join(msg_txt[2:])
+			# Next implementation of this should be a dict to handle regions (municipalities that are nearest to a particular clinic)
 			if location == 'HEBRON':
 				msg.respond('Al-Atqia Mosque, King Faisal Street, Hebron. 15-20 February 2014, 0800-1700')
 				return True
@@ -38,5 +41,9 @@ class Messaging(AppBase):
 			else:
 			# invalid or unknown locations
 			# flag to admin in DB for investigation and manual response
-				return False
+				msg.respond('Location unknown. Operation Smile will investigate and respond soon with more information.')
+				return True 
+
+		else:
+			return False
 
