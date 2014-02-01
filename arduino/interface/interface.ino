948c7f750c3583eee2d740cd265647138330ec3e
AmundsenJunior <scottedwardrussell@gmail.com>
2014-01-31 23:02:13 -0500

    Created Arduino interface sketch

---
+++ arduino/interface/interface.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+SoftwareSerial SIM900(7, 8);
+
+void setup()
+{
+  Serial.begin(19200);
+  SIM900.begin(19200);
+  SIM900power();
+  delay(20000);
+  
+  SIM900.print("AT+CMGF=1\r"); // set SMS mode to text
+  delay(100);
+  SIM900.print("AT+CNMI=2,2,0,0,0\r");
+  delay(100);
+}
+
+void SIM900power()
+{
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
+  delay(7000);
+}
+
+void loop()
+{
